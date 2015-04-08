e71deda7bb28f7be1fbe42fd28eaa1e9b69d5e93
Raoul George <raoulgeorge@gmail.com>
2015-04-08 14:15:30 +0530

    added jquery

---
+++ app/config/require-config.js
@@ -0,0 +1,8 @@
+(function () {
+    'use strict';
+    window.require = {
+        paths: {
+            'jquery': '../lib/jquery-2.1.3.min'
+        }
+    };
+}());
--- app/main.js
+++ app/main.js
@@ -1,3 +1,4 @@
-require([], function() {
-	
+require(['jquery'], function($) {
+
+	window.$ = $;
 });
