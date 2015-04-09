5785cc34ba0487f456a027261aa7c7795b78798d
Raoul George <raoulgeorge@gmail.com>
2015-04-09 10:41:52 +0530

    added composer

--- app/application/application.js
+++ app/application/application.js
@@ -2,7 +2,7 @@ define (function(require) {
 	'use strict';
 
 	function Application() {
-		console.log('constr');
+		
 	}
 
 	Application.prototype.start = function Application_start() {
---
+++ app/composer/composer.js
@@ -0,0 +1,15 @@
+/***
+The composer is responsible for creating views.
+Right now I am using it to 'compose' the scaffolding of the application
+
+***/
+define(function(require) {
+
+	'use strict';
+
+	function Composer() {
+
+	}
+
+	return Composer;
+});
